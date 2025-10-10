5fc5eb212f36b2829f70398deddea735b9e29cb6
JU Luyu <73172288+Luyu-Ju@users.noreply.github.com>
2025-10-10 16:54:49 +0800

    Delete code/DeepSource/unet directory

--- code/DeepSource/unet/matReader.m
+++
@@ -1,8 +0,0 @@
-%  matReader reads custom MAT files
-
-% Copyright 2017 The MathWorks, Inc.
-function data = matReader(filename)
-
-    d = load(filename);
-    f = fields(d);
-    data = d.(f{1})(:,:,:);
