fa4998590ece9a43930dc954e0afa0471f89bd6a
JU Luyu <73172288+Luyu-Ju@users.noreply.github.com>
2026-04-22 10:44:53 +0800

    Enhance matReader with comments and copyright
    Add documentation and copyright notice to matReader function

--- code/DeepSource/unet/matReader.m
+++ code/DeepSource/unet/matReader.m
@@ -1,1 +1,11 @@
+%  matReader reads custom MAT files
+%
+%  IMAGE = matReader(FILENAME) returns the channels of the
+%  Multispectral image saved in FILENAME.
 
+% Copyright 2017 The MathWorks, Inc.
+function data = matReader(filename)
+
+    d = load(filename);
+    f = fields(d);
+    data = d.(f{1})(:,:,:);
